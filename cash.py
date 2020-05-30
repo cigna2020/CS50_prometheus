69a1e16bf8c85dd6119453d7c9689fa56fe87583
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-05-30 21:21:18 +0300

    works only with the first part of the list 'money'

--- cash.py
+++ cash.py
@@ -1,1 +1,22 @@
-#start
+from typing import List
+
+
+def cash():
+    change = input('Numbers ')
+    money: List[float] = [0.25, 0.10, 0.5, 0.1]
+    counter = 0
+    while True:
+        if change.isnumeric():
+            break
+    while float(change) > 0:
+        if float(change) >= float(money[0]):
+            change = float(change) - money[0]
+            counter += 1
+            print(change, counter)
+
+
+cash()
+
+
+
+
