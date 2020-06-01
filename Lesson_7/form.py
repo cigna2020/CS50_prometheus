193f6d24113ec3cc523e90537622b878ea668554
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-01 16:05:01 +0300

    turned into a function with minimal validation

--- Lesson_7/form.py
+++ Lesson_7/form.py
@@ -1,12 +1,21 @@
 import csv
 
-username = input('Enter your name: ')
-userage = input('Enter your age: ')
-useraddress = input('Enter your address: ')
+def survey():
+    username = input('Enter your name: ')
+    while username.isdigit():
+        print('Please try to enter your name again: ')
+        username = input('Enter your name: ')
 
-file = open('register.csv', 'a')
+    userage = input('Enter your age: ')
+    while userage.isalpha():
+        print('Please try to enter your name again: ')
+        userage = input('Enter your age: ')
 
-writer = csv.writer(file)
-writer.writerow((username, userage, useraddress))
-file.close()
-print(str(file))
+    useraddress = input('Enter your address: ')
+
+    file = open('register.csv', 'a')
+    writer = csv.writer(file)
+    writer.writerow((username, userage, useraddress))
+    file.close()
+
+survey()
