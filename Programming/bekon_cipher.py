9e4a6269cadd27ac88ea4d2094ec04ba8518b4fe
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-04 17:27:02 +0300

    works correctly only on Python 3..

--- Programming/bekon_cipher.py
+++ Programming/bekon_cipher.py
@@ -1,15 +1,49 @@
+import sys
 
-key = 'aaaaabbbbbabbbaabbababbaaababaab'
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# Кодуємо через ab-послідовності:
-# p = abbab
-# r = babab
-# o = aabba
-# m = bbaab
-# e = abbbb
-# t = babba
-# h = bbbab
-# e = abbbb
-# u = abbaa
-# s = ababb
+def bekon_cipher():
+    key = 'aaaaabbbbbabbbaabbababbaaababaab'
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    a = str(sys.argv[1:])
+
+    for sign in a:
+        if sign == ' ':
+            sign = ''
+
+    step = ''
+    for i in a:
+        if i.islower():
+            step = step + 'a'
+        elif i.isupper():
+            step = step + 'b'
+
+    part = str
+    list = []
+    start = 0
+    end = 5
+    for i in alphabet:
+        part = key[start:end]
+        list.append(part)
+        start = start + 1
+        end = end + 1
+
+    start_1 = 0
+    start_2 = 5
+    list_2 = ()
+    list_3 = ''
+    result = ''
+    length = ''
+    j = 1
+    while j < len(a):
+        length = step[start_1:start_2]
+        if len(length) == 5:
+            list_2 = list.index(length)
+            list_3 = alphabet[list_2]
+            result = result + list_3
+        j = j + 1
+        start_1 = start_1 + 5
+        start_2 = start_2 + 5
+
+    print(result)
+
+bekon_cipher()
