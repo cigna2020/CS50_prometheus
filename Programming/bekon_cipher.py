ab3137ee3bf9aec79f7ac584d85d235342b5bd65
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-04 09:35:39 +0300

    an initial commit

---
+++ Programming/bekon_cipher.py
@@ -0,0 +1,15 @@
+
+key = 'aaaaabbbbbabbbaabbababbaaababaab'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# Кодуємо через ab-послідовності:
+# p = abbab
+# r = babab
+# o = aabba
+# m = bbaab
+# e = abbbb
+# t = babba
+# h = bbbab
+# e = abbbb
+# u = abbaa
+# s = ababb
