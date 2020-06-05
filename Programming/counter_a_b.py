73b4b923d4721a8c4b12f9369a8dccd8b7128933
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-05 18:51:31 +0300

    returns count of the same values in lists a and b with unique values.

--- Programming/counter_a_b.py
+++ Programming/counter_a_b.py
@@ -3,6 +3,8 @@
 def counter_a_b(a, b):
     unique_list_a = []
     unique_list_b = []
+    counter = 0
+
     for i in str(a):
         if i not in unique_list_a:
             unique_list_a.append(i)
@@ -11,4 +13,10 @@ def counter_a_b(a, b):
             unique_list_b.append(j)
     print(unique_list_b, unique_list_a)
 
-counter_a_b(1233211, 12128)
+    for d in unique_list_a:
+        if d in unique_list_b:
+            counter += 1
+    print(counter)
+
+
+counter_a_b(12345, 567)
