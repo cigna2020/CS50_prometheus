5f90a2bc1e2828c3f3266e4d215c9b5d72ffd0ea
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-05 16:52:04 +0300

    initial commit

---
+++ Programming/clean_list.py
@@ -0,0 +1,12 @@
+
+def clean_list(list):
+    new_list = []
+    for i in list:
+        if i not in list:
+            new_list.(i)
+        # return new_list
+    print(new_list)
+
+clean_list([32, 32.1, 32.0, -123])
+
+
