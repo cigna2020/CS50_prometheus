a4c377eeb560eda45b129aff32dcbbfb45d205c7
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-07 16:26:03 +0300

    in 'def make_lab' added 2 conditions

--- Programming/student_and_labs.py
+++ Programming/student_and_labs.py
@@ -2,15 +2,22 @@ class Student():
     name = None
     config = None
     marks = None
+    n = 0
 
     def __init__(self, name, config):
         self.name = name
         self.config = config
         self.marks = {}
-        for i in range(1, self.config['lab_num'] + 1):
+        for i in range(0, self.config['lab_num']):
             self.marks[i] = 0
 
-    def make_lab(self, m, n=1):
+    def make_lab(self, m, n=n):
+        if n == n:
+            if self.marks.get(n) < m:
+                if float(m) > float(self.config['lab_max']):
+                    self.marks[n] = self.config['lab_max']
+                while self.marks.get(n) != 0:
+                    n += 1
         if float(m) > float(self.config['lab_max']):
             self.marks[n] = self.config['lab_max']
         else:
@@ -30,3 +37,23 @@ class Student():
             return (sum(self.marks.values()), True)
         else:
             return (sum(self.marks.values()), False)
+
+
+conf = {
+'exam_max': 30,
+'lab_max': 7,
+'lab_num': 10,
+'k': 0.61,
+}
+oleg = Student('Oleg', conf)
+oleg.make_lab(1)     # labs: 1 0 0 0 0 0 0 0 0 0, exam: 0
+oleg.make_lab(8, 0) # labs: 7 0 0 0 0 0 0 0 0 0, exam: 0
+oleg.make_lab(1) # labs: 7 1 0 0 0 0 0 0 0 0, exam: 0
+oleg.make_lab(10, 7) # labs: 7 1 0 0 0 0 0 7 0 0, exam: 0
+oleg.make_lab(4, 1) # labs: 7 4 0 0 0 0 0 7 0 0, exam: 0
+oleg.make_lab(5) # labs: 7 4 5 0 0 0 0 7 0 0, exam: 0
+oleg.make_lab(6.5) # labs: 7 4 5 6.5 0 0 0 7 0 0, exam: 0
+oleg.make_exam(32) # labs: 7 4 5 6.5 0 0 0 7 0 0, exam: 30
+print(oleg.is_certified()) # (59.5, False)
+oleg.make_lab(7, 1) # labs: 7 7 5 6.5 0 0 0 7 0 0, exam: 30
+print(oleg.is_certified()) # (62.5, True)
